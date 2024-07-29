13307850ac8f984230ec9bd7966d4c24eb304153
Zohaibaay <muhammadzohaibmalik10@gmail.com>
2024-07-30 00:41:01 +0500

    final Commit

---
+++ app/src/main/java/com/example/assignment3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.assignment3;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Tab 1");
+                            break;
+                        case 1:
+                            tab.setText("Tab 2");
+                            break;
+                    }
+                }).attach();
+    }
+}
---
+++ app/src/main/java/com/example/assignment3/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.example.assignment3;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            default:
+                throw new IllegalStateException("Invalid position " + position);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2; // Number of tabs
+    }
+}
